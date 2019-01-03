26ff82aba1416b6d06a3026a41fe39cdf0c8b965
wekwek <1520538430@qq.com>
2019-01-03 15:35:10 +0800

    Update xrd_graphs.py

--- xrd_graphs.py
+++ xrd_graphs.py
@@ -10,25 +10,29 @@ def file_list(dirpath):
 	for i in range(num):
 		file_path.append(dirpath+file_list[i])
 		file_name.append(file_list[i].split('.')[0])
-	return num,file_path,file_name
+	return file_path,file_name
 
-def xrd_graph(num,file_path,file_name):
+def xrd_graph(file_path,file_name):
+	num=len(file_name)
 	if num%2==1:
 		num=num+1
 	else:
 		pass
-	fig,axes=plt.subplots(2,int(num/2))
+	fig=plt.figure()
 	for i in range(num):
 		columns=['2Theta','Intensity']
 		data=pd.read_table(file_path[i],sep='\s+',skiprows=31,header=None,names=columns)
-		ax(2,num/2,i).plot(data['2Theta'],data['Intensity'],'k')
+		ax=fig.add_subplot(2,int(num/2),i+1)
+		ax.plot(data['2Theta'],data['Intensity'],'k')
 		ax.set_title(file_name[i])
 		ax.set_xlabel('2Theta')
 		ax.set_ylabel('Intensity')
+		plt.show()
+
 def main():
 	dirpath='E:/MOF-B/'
-	num,file_path,file_name=file_list(dirpath)
-	xrd_graph(num,file_path,file_name)
+	file_path,file_name=file_list(dirpath)
+	xrd_graph(file_path,file_name)
 
 if __name__ == '__main__':
 	main()
